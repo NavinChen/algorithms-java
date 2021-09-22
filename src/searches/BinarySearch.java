0886af143e7f6f686e35d96d5011b1b93a9a88a4
neo <neochen13@foxmail.com>
2021-09-22 13:31:39 +0800

    binary search

---
+++ src/searches/BinarySearch.java
@@ -0,0 +1,35 @@
+package searches;
+
+/**
+ * @author: neo
+ * @description: 二分查找
+ */
+public class BinarySearch {
+
+    public static int search(int[] array, int element) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            int guess = array[middle];
+
+            if (guess == element) {
+                return middle;
+            } else if (guess > element) {
+                high = guess - 1;
+            } else {
+                low = guess + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int index = BinarySearch.search(array, 3);
+        System.out.println("index => " + index);
+    }
+
+}
